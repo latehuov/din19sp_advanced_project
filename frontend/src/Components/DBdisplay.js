ae2c81c51a35f41cb9a53170f346cf8d40f2f119
Dmitrii-Weaver <cordies1106@gmail.com>
2020-11-05 16:29:57 +0300

    restaurant opening hours working properly

--- frontend/src/Components/DBdisplay.js
+++ frontend/src/Components/DBdisplay.js
@@ -1,5 +1,44 @@
 import React from 'react'
 import styles from './DBdisplay.css'
+
+var today = new Date()
+var hours = today.getHours()
+var minutes = today.getMinutes()
+
+function checkIfOpen(open, close) {
+    let openHours = open.substring(0, 2)
+    parseInt(openHours)
+    let closeHours = close.substring(0, 2)
+    parseInt(closeHours)
+    if (openHours < hours && hours < closeHours) {
+        return "open"
+    }
+    else if (openHours == hours) {
+        let openMinutes = open.substring(3, 5)
+        parseInt(openMinutes)
+        console.log(openMinutes, minutes)
+        if (openMinutes < minutes) {
+            return "open"
+        }
+        else if (openMinutes > minutes) {
+            return "closed"
+        }
+    } 
+    else if (closeHours == hours) {
+        let closeMinutes = close.substring(3, 5)
+        parseInt(closeMinutes)
+        if (closeMinutes > minutes) {
+            return "open"
+        }
+        else {
+            return "closed"
+        }
+    }
+    else if (openHours < hours && hours > closeHours){
+        return "closed"
+    }
+}
+
 export default function DBdisplay(props) {
     return (
         <div class={styles.dataBox}>
@@ -8,8 +47,7 @@ export default function DBdisplay(props) {
                     props.data.map(item => <tr class={styles.aRow}>
                         <td>{item.name_res}</td>
                         <td>{item.address}</td>
-                        <td>{item.time_open}</td>
-                        <td>{item.time_close}</td>
+                        <td>{checkIfOpen(item.time_open, item.time_close)}</td>
                     </tr>)
                 }
             </table>
