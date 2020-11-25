5322d7e6d35c12cfb1f97657febece4a4da64efe
Dmitrii-Weaver <cordies1106@gmail.com>
2020-11-25 14:35:39 +0300

    pics are now stored locally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import DBdisplay from './Components/DBdisplay';
 import axios from 'axios';
+import pic from "./Genshin-Impact-Noelle.png"
+import gif from "./1507938144_tumblr_owx0emJxdR1tvl583o2_r1_1280.gif"
 
 let fakeData = require('./example.json')
 
@@ -36,10 +38,11 @@ class App extends Component {
       <div class="mainDisplay">
         <h1 class="theTitle">localhostess3000</h1>
         <div id="NoelleBox">
-          <img id="Noelle" src="https://owwya.com/wp-content/uploads/2020/10/Genshin-Impact-Noelle.png"></img>
+          <img id="Noelle" src={pic} alt="aaa"></img>
         </div>
         <br></br>
         <DBdisplay data={this.state.data} setSearchResult={this.setSearchResult} SearchResult={this.state.SearchResult}/>
+        <img id="theGif" src={gif} alt="aaa"></img>
       </div>
     )
   }
